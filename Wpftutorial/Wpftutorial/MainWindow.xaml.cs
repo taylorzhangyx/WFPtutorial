b33017003051935cbd2f2636a7ccaaba9b4751f6
Yuxin Zhang <taylorzhangyx@gmail.com>
2017-06-14 17:38:17 -0700

    focus change record

--- Wpftutorial/Wpftutorial/MainWindow.xaml.cs
+++ Wpftutorial/Wpftutorial/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Automation;
+using Wpftutorial.methods;
+using System.Runtime.InteropServices;
 
 namespace Wpftutorial
 {
@@ -20,6 +14,13 @@ namespace Wpftutorial
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,9 +28,52 @@ namespace Wpftutorial
             this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
 
+        private Automation​Focus​Changed​Event autofocus = new Automation​Focus​Changed​Event();
+
         private void Window_MouseMove(object sender, MouseEventArgs e)
         {
             Title = e.GetPosition(this).ToString();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            autofocus.SubscribeToFocusChange(this);
+        }
+
+        private void rmvButton_Click(object sender, RoutedEventArgs e)
+        {
+            autofocus.UnsubscribeFocusChange(this);
+        }
+
+
+
+        /// <summary>
+        /// Handle the event.
+        /// </summary>
+        /// <param name="src">Object that raised the event.</param>
+        /// <param name="e">Event arguments.</param>
+        public void OnFocusChange(object src, AutomationFocusChangedEventArgs e)
+        {
+            // TODO Add event handling code.
+            // The arguments tell you which elements have lost and received focus.
+            this.Dispatcher.Invoke(() =>
+            {
+                //focushistory.Text += autofocus.getprocessinfo();
+                string name = gettitle(GetForegroundWindow());
+                focushistory.Text = name;
+            });
+        }
+
+        private string gettitle(IntPtr prt)
+        {
+            const int count = 512;
+            var text = new StringBuilder(count);
+
+            if (GetWindowText(prt, text, count) > 0)
+            {
+                return text.ToString();
+            }
+            return "NULL";
         }
     }
 }
---
+++ Wpftutorial/Wpftutorial/methods/Automation​Focus​Changed​Event.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Windows.Automation;
+using System.Diagnostics;
+using Wpftutorial;
+using System;
+using System.ComponentModel;
+
+namespace Wpftutorial.methods
+{
+    class Automation​Focus​Changed​Event
+    {
+        AutomationFocusChangedEventHandler focusHandler = null;
+
+        /// <summary>
+        /// Create an event handler and register it.
+        /// </summary>
+        public void SubscribeToFocusChange(MainWindow window)
+        {
+            focusHandler = new AutomationFocusChangedEventHandler(window.OnFocusChange);
+            Automation.AddAutomationFocusChangedEventHandler(focusHandler);
+        }
+
+
+        /// <summary>
+        /// Cancel subscription to the event.
+        /// </summary>
+        public void UnsubscribeFocusChange(MainWindow window)
+        {
+            if (focusHandler != null)
+            {
+                Automation.RemoveAutomationFocusChangedEventHandler(window.OnFocusChange);
+            }
+        }
+
+        public string getprocessinfo()
+        {
+
+            return Process.GetCurrentProcess().ProcessName;
+        }
+    }
+}
